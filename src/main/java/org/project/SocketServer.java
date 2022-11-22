7716e64d85acf40981bdbb859602f3d12b2cc479
Bucksin <bucksin8@gmail.com>
2022-11-21 17:01:31 -0800

    included changes in window.java

--- src/main/java/org/project/SocketServer.java
+++ src/main/java/org/project/SocketServer.java
@@ -1,2 +1,62 @@
-package org.project;public class SocketServer {
+package org.project;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.ClassNotFoundException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+
+public class SocketServer {
+
+  // static ServerSocket variable
+  private static ServerSocket server;
+
+  // socket server port on which it will listen
+  private static int port = 9876;
+
+  public static void main(String args[]) throws IOException, ClassNotFoundException, InterruptedException {
+    // create the socket server object
+    server = new ServerSocket(port);
+    Random random = new Random();
+
+    // keep listens indefinitely until receives 'exit' call or program terminates
+    while(true){
+      Thread.sleep(random.nextInt(5000));
+
+      System.out.println("Waiting for the client request");
+
+      // creating socket and waiting for client connection
+      Socket socket = server.accept();
+
+      // read from socket to ObjectInputStream object
+      ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+
+      // convert ObjectInputStream object to String
+      String message = (String) ois.readObject();
+      System.out.println("Message Received Serverside: " + message);
+      int total = Integer.parseInt(message);
+
+
+      // create ObjectOutputStream object
+      ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+
+      // write object to Socket
+      oos.writeObject("Hi Client "+total);
+
+      // close resources
+      ois.close();
+      oos.close();
+      socket.close();
+
+      // terminate the server if client sends exit request
+      if(message.equalsIgnoreCase("exit")) break;
+    }
+    System.out.println("Shutting down Socket server!!");
+
+    // close the ServerSocket object
+    server.close();
+  }
+
 }
--- src/main/java/org/project/Window.java
+++ src/main/java/org/project/Window.java
@@ -1,6 +1,7 @@
 package org.project;
 
 import processing.core.PApplet;
+import processing.event.KeyEvent;
 
 import java.util.ArrayList;
 
@@ -8,11 +9,64 @@ public class Window extends PApplet{
   private Player player;
   private ArrayList<Enemy> enemies;
 
-  public void setup() {
-
+  /**
+   * Sets the size of the applet window.
+   */
+  public void settings() {
+    size(640, 360);
   }
 
+  public void setup() {
+    player = Player.getInstance(this);
+  }
+
+  public void draw() {
+    player.draw();
+    background(0);
+  }
+
+  /**
+   * Handles arrowkey inputs for player movement.
+   *
+   * @param key **keypress event object**
+   */
+  public void keyPressed(KeyEvent key) {
+//    if (player == null) {
+//      return;
+//    } else {
+//      switch (key.getKeyCode()) {
+//        case RIGHT:
+//          player.setDirection(0);
+//          player.move();
+//          break;
+//        case LEFT:
+//          player.setDirection(1);
+//          player.move();
+//          break;
+//        case DOWN:
+//          player.setDirection(2);
+//
+//          player.move();
+//          break;
+//        case UP:
+//          player.setDirection(3);
+//
+//          player.move();
+//          break;
+//        default:
+//          break;
+//      }
+//    }
+  }
+
+  /**
+   * Main function.
+   *
+   * @param args arguments from command line
+   */
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    String[] appletArgs = new String[]{"window"};
+    Window window = new Window();
+    PApplet.runSketch(appletArgs, window);
   }
 }
